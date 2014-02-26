bb1a7f46623cbcae97f3ac9f5388698fb49a927c
lrita <luismestrerita@gmail.com>
2014-02-26 17:59:48 +0000

    cufon includes for over effect

--- includes/cufon.php
+++ includes/cufon.php
@@ -1,3 +1,5 @@
 <script type="text/javascript">
-        Cufon.replace('.navi, .newsHeader, .storiesHeader, .storiesFooter');
+Cufon.replace('#suckerfishnav li li a', {
+    hover: { color: '#3e3e3e', 'backgorund-color': '#fa9f08'},
+});
 </script>
